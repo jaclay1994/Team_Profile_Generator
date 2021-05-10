0265e6c0abc8757588581ee43ec9a224c224602d
jaclay1994 <jaclay94@gmail.com>
2021-05-10 11:06:44 -0400

    Added object reference and connection to HTML.

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const Engineer = require("./lib/engineer")
-// const Employee = require("./lib/employee");
-const util = require("util");
+const { _typeOf } = require("jest-mock");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+// const Engineer = require("./lib/engineer")
+const Employee = require("./lib/employee");
+// const util = require("util");
 // const { writeFileSync } = require("node:fs");
 // const Engineer = require("./lib/engineer");
 // const { choices } = require("yargs");
 // const { start } = require("node:repl");
 
-const writeFileAsync = util.promisify(fs.writeFile);
+// const writeFileAsync = util.promisify(fs.writeFile);
 
-const html = [``]
+let html = ""
 
-function createProfile() {
+async function createProfile() {
+    html = html.concat(createHTML());
     return inquirer.prompt([
         {
             type: "input",
@@ -33,41 +37,116 @@ function createProfile() {
             type: "input",
             name: "officeNumber",
             message: "Enter manager's office number."
-        },
-        {
-            type: "list",
-            message: "Choose to add an employee or finish team profile.",
-            name: "mainMenu",
-            choices: [
-                "Add Engineer",
-                "Add Intern",
-                "Finish & Application"
-            ]
         }
-    ]).then(function(answers) {
-        if (answers.mainMenu === "Add Engineer") {
-            addEngineer()
-            // html.push(engineerHTML(engineers));
-        }
+    ]).then(function mainMenu() {
+        inquirer.prompt([
+            {
+                type: "list",
+                message: "Choose to add an employee or finish team profile.",
+                name: "mainMenu",
+                choices: [
+                    "Add Engineer",
+                    "Add Intern",
+                    "Finish & Application"
+                ]
+            }
+        ]).then(userChoice => {
+            switch (userChoice.mainMenu) {
+                case "Add Engineer":
+                    function addEngineer() {
+                        inquirer.prompt([
+                            {
+                                type: "input",
+                                name: "name",
+                                message: "What is the engineer's name?"
+                            },
+                            {
+                                type: "input",
+                                name: "id",
+                                message: "Enter engineer's employee ID."
+                            },
+                            {
+                                type: "input",
+                                name: "email",
+                                message: "Enter engineer's email address."
+                            },
+                            {
+                                type: "input",
+                                name: "github",
+                                message: "Enter engineer's github."
+                            }
+                        ]).then(function (answers) {
+                            console.log(answers)
+                            const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github)
+                            console.log(engineer)
+                            engineerHTML(engineer)
+                            html = html.concat(engineerHTML(engineer))
+                            //   const engineer = new Engineer ({})  
+                            mainMenu()
+
+                        })
+                    };
+                    addEngineer();
+                    break;
+                case "Add Intern":
+                    function addIntern() {
+                        inquirer.prompt([
+                            {
+                                type: "input",
+                                name: "internname",
+                                message: "What is the intern's name?"
+                            },
+                            {
+                                type: "input",
+                                name: "internemployeeId",
+                                message: "Enter intern's employee ID."
+                            },
+                            {
+                                type: "input",
+                                name: "internemail",
+                                message: "Enter intern's email address."
+                            },
+                            {
+                                type: "input",
+                                name: "school",
+                                message: "Enter intern's school."
+                            }
+                        ]).then(function (answers) {
+                            const intern = new Intern(answers.internname, answers.internemployeeid, answers.internemail, answers.school)
+                            console.log(intern)
+                            engineerHTML(intern)
+                            html = html.concat(internHTML(intern))
+                            //   const engineer = new Engineer ({})  
+                            mainMenu()
+                        })
+                    };
+                    addIntern();
+                    break;
+
+                case "Finish & Application":
+                    try {
+                        console.log(typeof html)
+                        console.log("Success!")
+                        html = html.concat(endHTML());
+                        console.log(typeof html)
+
+
+                        fs.writeFile("index.html", html, err => {
+                            if (err) {
+                                console.error(err)
+                                return
+                            }
+                        });
+                    } catch (err) {
+                        console.log(err);
+                    }
+
+                default:
+                    return
+            }
+        })
     })
 }
-
-// async function cont() {
-//     await inquirer.prompt([
-//         {
-//             type: "list",
-//             message: "Choose to add an employee or finish team profile.",
-//             name: "mainMenu",
-//             choices: [
-//                 "Add Engineer",
-//                 "Add Intern",
-//                 "Finish & Application"
-//             ]
-//         }
-//     ]).then(function(answers) {
-//         return answers = this.mainMenu
-//     })
-// }
 
 function createHTML() {
 
@@ -96,74 +175,49 @@ function createHTML() {
     `
 }
 
-function addEngineer() {
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "engineername",
-            message: "What is the engineer's name?"
-        },
-        {
-            type: "input",
-            name: "engineeremployeeId",
-            message: "Enter engineer's employee ID."
-        },
-        {
-            type: "input",
-            name: "engineeremail",
-            message: "Enter engineer's email address."
-        },
-        {
-            type: "input",
-            name: "github",
-            message: "Enter engineer's github."
-        }
-    ])
-}
-
-function engineerHTML(engineers) {
+function engineerHTML(engineer) {
     // const engineers = new Engineer(id, name, email, github, getRole());
     // cont()
     return `
     <div class="col-sm-4">
                 <div class="card" style="width: 18rem;">
                     <div class="card-body">
-                        <h5 class="card-title">${engineers.engineername}</h5>
+                        <h5 class="card-title">${engineer.name}</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of
                             the                                card's
                              content.</p>
                     </div>
                     <ul class="list-group list-group-flush">
-                        <li class="list-group-item">Employee ID: ${engineers.engineeremployeeId}</li>
-                        <li class="list-group-item">Email: ${engineers.engineeremail}</li>
-                        <li class="list-group-item">Github: ${engineers.github}</li>
+                        <li class="list-group-item">Employee ID: ${engineer.id}</li>
+                        <li class="list-group-item">Email: ${engineer.email}</li>
+                        <li class="list-group-item">Github: ${engineer.github}</li>
                       </ul>
                     </div>
                 </div>
     `;
 }
 
-// function addIntern(interns) {
-//     return `
-//     <div class="col-sm-4">
-//                 <div class="card" style="width: 18rem;">
-//                     <div class="card-body">
-//                         <h5 class="card-title">${interns.start}</h5>
-//                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-//                             the                                card's
-//                              content.</p>
-//                     </div>
-//                     <ul class="list-group list-group-flush">
-//                         <li class="list-group-item">Employee ID: ${interns.employeeId}</li>
-//                         <li class="list-group-item">Email: ${interns.email}</li>
-//                         <li class="list-group-item">Office #: ${interns.officeNumber}</li>
-//                         <li class="list-group-item">Office #: ${interns.officeNumber}</li>                        </ul>
-//                     </div>
-//                 </div>
-//     `
-// }
+function internHTML(interns) {
+    return `
+    <div class="col-sm-4">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">${interns.start}</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the                                card's
+                             content.</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Employee ID: ${interns.employeeId}</li>
+                        <li class="list-group-item">Email: ${interns.email}</li>
+                        <li class="list-group-item">Office #: ${interns.officeNumber}</li>
+                        <li class="list-group-item">Office #: ${interns.officeNumber}</li>                        </ul>
+                    </div>
+                </div>
+    `
+}
 
-async function endHTML() {
+function endHTML() {
     return `
             </div>
         </div>
@@ -176,31 +230,5 @@ async function endHTML() {
     </html>
     `
 }
+
 createProfile()
-    .then(function (answers, engineers) {
-        
-        html.push(createHTML(answers))
-        // html.push(engineerHTML(engineers));
-        
-
-        
-        // let employeeName = data.start
-        // let employeeId = data.employeeId
-        // let employeeEmail = data.email
-        // let employeeOfficeNumber = data.officeNumber
-        // const html = createHTML(answers);
-        // if (answers.mainMenu === "Finish & Application") {
-        //     cont()
-        // }
-        // html.push(endHTML())
-        return writeFileAsync("index.html", html);
-    })
-    .catch(function (err) {
-        console.log(err);
-    });
-// async function init() {
-//     try {
-//         const
-//     }
-// }
-
